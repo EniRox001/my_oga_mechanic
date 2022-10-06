681045f4eeaf4711a9c7a9bb04d35a7d5fbc79af
enirox <enirox001@gmail.com>
2022-10-06 12:06:42 +0100

    completed vehicle profile

--- lib/screens/menu_vehicle_profile.dart
+++ lib/screens/menu_vehicle_profile.dart
@@ -5,6 +5,60 @@ class MenuVehicleProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('vehicle profile');
+    return Scaffold(
+      body: BackgroundWidget(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(
+              32.0.sp,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'vehicle profile'.toUpperCase(),
+                  style: CustomTextStyle().largeText,
+                ),
+                Column(
+                  children: const [
+                    WMechanicData(title: 'CAR ID', text: 'CP4349289'),
+                    WMechanicData(title: 'MAKE', text: 'AUDI'),
+                    WMechanicData(title: 'MODEL', text: 'A6'),
+                    WMechanicData(title: 'YEAR', text: '2012'),
+                  ],
+                ),
+                const WSelectedCarWidget(),
+                Column(
+                  children: const [
+                    WMechanicData(title: 'ROAD WORTHINESS', text: '22-08-2023'),
+                    WMechanicData(
+                      title: 'INSURANCE',
+                      text: '22-08-2023',
+                    ),
+                  ],
+                ),
+                WTextButton(
+                  onPressed: () {},
+                  text: 'premium gold',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
